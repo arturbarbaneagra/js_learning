82c9b8d92a697844ac15eb08ed8fbe47a61bd94e
Artur Barbaneagra <arturbarbaneagra@gmail.com>
2022-01-21 00:44:16 +0200

    Am adaugat Task9 si Task10

---
+++ poo_basics/Task10/app.js
@@ -0,0 +1,13 @@
+const dividers = (number) => {
+  if (number > 0) {
+    for (let i = 1; i <= number; i++) {
+      if (number % i == 0) {
+        console.log(i);
+      }
+    }
+  } else {
+    alert("Please input an positive number")
+  }
+}
+
+dividers(prompt("Input the number"));18
---
+++ poo_basics/Task9/app.js
@@ -0,0 +1,7 @@
+let str = prompt("Hello, input your name please");
+alert(`Hi ${str}`);
+if (confirm("Do you know JS?")) {
+  alert("Nice!")
+} else {
+  alert("You can learn it right now")
+}
