3cdc045743edd8c9838db9ad1fdbf6a48b493e79
Artur Barbaneagra <arturbarbaneagra@gmail.com>
2022-01-20 23:46:10 +0200

    Am adaugat Task7

---
+++ poo_basics/Task7/app.js
@@ -0,0 +1,7 @@
+let age = prompt("Introdu age");
+if ((age <= 14)||(age >= 90)) {
+  alert("True")
+} else {alert("False")}
+if (!((age >= 14)&&(age <= 90))) {
+  alert("True")
+} else {alert("False")}
