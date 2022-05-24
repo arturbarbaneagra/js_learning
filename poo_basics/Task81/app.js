b05ac82ebde87c6c1cbfaf2ffef918044c00c48b
barbaneagra <106161155+barbaneagra@users.noreply.github.com>
2022-05-24 16:32:59 +0300

    Update app.js

--- poo_basics/Task81/app.js
+++ poo_basics/Task81/app.js
@@ -4,24 +4,14 @@ function ageCalculator() {
   if(userinput==null || userinput=='') {  
     document.getElementById("message").innerHTML = "**Choose a date please!";    
     return false;   
-  } else {  
-    
-  //calculate month difference from current date in time  
-  var month_diff = Date.now() - dob.getTime();  
-    
-  //convert the calculated difference in date format  
-  var age_dt = new Date(month_diff);   
-
-  //extract year from date      
+  } else {    
+  var month_diff = Date.now() - dob.getTime();    
+  var age_dt = new Date(month_diff);       
   var year = age_dt.getUTCFullYear();
   var month = age_dt.getUTCMonth();
-  var day = age_dt.getUTCDay();  
-    
-  //now calculate the age of the user  
+  var day = age_dt.getUTCDay();   
   var age_y = Math.abs(year - 1970);
   var age_m = month + age_y * 12;
-    
-  //display the calculated age  
   return document.getElementById("result").innerHTML =    
            "Age is: " + age_y + " years " + " or " + age_m + " months ";  
   }  
